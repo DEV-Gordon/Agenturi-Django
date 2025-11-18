739e28b05299e7663936d35847b116e1f1c55a18
Carlos Zarate <128831952+DEV-Gordon@users.noreply.github.com>
2025-11-17 23:49:51 -0500

    Update serializers.py

--- apps/customers/serializers.py
+++ apps/customers/serializers.py
@@ -15,7 +15,7 @@ class BookingActivitySerializer(serializers.ModelSerializer):
         model = BookingActivity
         fields = ['id', 'activity', 'booking']
 
-
+'''
 class BookingSerializer(serializers.ModelSerializer):
     plan = PlanSerializer(read_only=True)
     customer = CustomerSerializer(read_only=True)
@@ -23,8 +23,31 @@ class BookingSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Booking
-        fields = ['id', 'booking_date', 'status', 'plan', 'customer', 'activities']
+        fields = ['id', 'booking_date', 'status', 'plan', 'customer', 'activities'] '''
 
+class BookingSerializer(serializers.ModelSerializer):
+    plan_id = serializers.IntegerField(write_only=True)
+    customer_id = serializers.IntegerField(write_only=True)
+
+    plan = PlanSerializer(read_only=True)
+    customer = CustomerSerializer(read_only=True)
+
+    activities = BookingActivitySerializer(
+        many=True,
+        read_only=True,
+        source='bookingactivity_set'
+    )
+
+    class Meta:
+        model = Booking
+        fields = [
+            'id',
+            'booking_date',
+            'status',
+            'plan', 'plan_id',
+            'customer', 'customer_id',
+            'activities'
+        ]
 
 class PaymentSerializer(serializers.ModelSerializer):
     booking = BookingSerializer(read_only=True)
